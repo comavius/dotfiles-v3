a7e50aac1793da21c31739e0aaf9f4b72aadc108
comavius <comavius@proton.me>
2026-07-13 22:31:40 +0900

    start using nixos-cuda cache server

--- nixos/system.nix
+++ nixos/system.nix
@@ -56,10 +56,18 @@ in
     networkmanager.enable = true;
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos-cuda.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+    ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 
   system = {
     configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
